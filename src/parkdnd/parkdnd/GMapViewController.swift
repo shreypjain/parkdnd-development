1d1330c7e0f9fd88349d0630e7760a3b1a8fc1a0
Shrey Jain <jainshreyp@gmail.com>
2020-07-12 22:16:42 -0400

    finished adding some google maps api work

--- src/parkdnd/parkdnd/GMapViewController.swift
+++ src/parkdnd/parkdnd/GMapViewController.swift
@@ -18,15 +18,15 @@ class GMapViewController: UIViewController {
         // Do any additional setup after loading the view.
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: 40.456420, longitude: -74.493510, zoom: 15.0)
         let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
         self.view.addSubview(mapView)
 
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2D(latitude: 40.456420, longitude: -74.493510)
+        marker.title = "North Brunswick"
+        marker.snippet = "New Jersey"
         marker.map = mapView
 
     }
